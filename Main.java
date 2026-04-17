f295e036988ed70bb4c158b65ee03faf2a28a2d0
Terres06 <pedrocoutoterres@gmail.com>
2026-04-17 17:33:40 -0300

    feat: create main program class

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        // Cria composição
+        ComposicaoFerroviaria cf = new ComposicaoFerroviaria(100, "composicao.dat");
+
+        System.out.println("=== TESTE CASO 1: Criar composição padrão ===");
+
+        System.out.println("Composição após criação:");
+        System.out.println(cf.toString()); // usa o toString do Deque
+
+        System.out.println("\n=== TESTE CASO 2: Inserir vagões ===");
+
+        // Inserindo uma locomotiva no início
+        Locomotiva loco = new Locomotiva(20, 150, 4000);
+        cf.inserirInicio(loco);
+
+        System.out.println("Composição após inserções:");
+        System.out.println(cf.toString());
+
+        System.out.println("\nPrimeiro vagão:");
+        Vagao primeiro = (Vagao) cf.peekFront();
+        if (primeiro != null) primeiro.imprime();
+
+        System.out.println("\nÚltimo vagão:");
+        Vagao ultimo = (Vagao) cf.peekRear();
+        if (ultimo != null) ultimo.imprime();
     }
-    
 }
